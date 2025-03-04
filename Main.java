436a17300348b7492214f4424dc1ffcadd9eec80
Cavid2002 <quliyevcavid132@gmail.com>
2025-03-05 02:00:24 +0400

    report done

--- Main.java
+++ Main.java
@@ -3,10 +3,15 @@ public class Main
 {
     public static void main(String[] args)
     {
-        GCWithAc3 d = new GCWithAc3("./samples/sample7.txt");
+        GCWithAc3 d = new GCWithAc3("./samples/sample4.txt");
 
-        d.solve();
+        boolean res = d.solve();
 
+        if(res == false)
+        {
+            System.out.println("Impossible");
+            System.exit(1);
+        }
         
         d.printColorContent();
     }
